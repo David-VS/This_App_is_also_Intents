d5ce2b6dd45fb20f01084e0509a187117187c067
David-VS <63110701+David-VS@users.noreply.github.com>
2023-10-09 08:48:43 +0200

    Initial commit

---
+++ app/src/main/java/be/ehb/thisappisalsointents/MainActivity.java
@@ -0,0 +1,21 @@
+package be.ehb.thisappisalsointents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import be.ehb.thisappisalsointents.fragments.Fragment9;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frag_container, new Fragment9())
+                .commit();
+    }
+}
